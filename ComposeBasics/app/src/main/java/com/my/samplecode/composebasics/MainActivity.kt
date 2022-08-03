1dae522e2843ef1a6f29670d463a6e4dd2ad350b
김재희 <jaehee.kim@hyundai.com>
2022-08-03 16:52:13 +0900

    animation + state 구현

--- ComposeBasics/app/src/main/java/com/my/samplecode/composebasics/MainActivity.kt
+++ ComposeBasics/app/src/main/java/com/my/samplecode/composebasics/MainActivity.kt
@@ -3,9 +3,16 @@ package com.my.samplecode.composebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,14 +30,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(names: List<String> = listOf("World", "Compose")) {
+fun MyApp() {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinuedClicked = {
+            shouldShowOnboarding = false
+        })
+    } else {
+        Greetings()
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun Greetings(names: List<String> = List(100) { "$it" }) {
     // A surface container using the 'background' color from the theme
     Surface(
         color = MaterialTheme.colors.background
     ) {
-        Column (modifier = Modifier.padding(vertical = 4.dp)){
-            for (name in names) {
-                Greeting(name)
+        Column (modifier = Modifier.padding(vertical = 4.dp)) {
+            LazyColumn {
+                item { Text(text = "Header") }
+                stickyHeader { Text(text = "stickyHeader") }
+                items(names) { name ->
+                    Greeting(name)
+                }
             }
         }
     }
@@ -38,20 +63,30 @@ fun MyApp(names: List<String> = listOf("World", "Compose")) {
 
 @Composable
 fun Greeting(name: String) {
+    var expanded by remember { mutableStateOf(false)}
+    val extraPadding by animateDpAsState(
+        targetValue = if (expanded) 48.dp else 0.dp,
+        animationSpec = tween(
+            durationMillis = 2000
+        )
+    )
     Surface(
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
         color = MaterialTheme.colors.primary
     ) {
-        Row (modifier = Modifier.padding(24.dp)){
+        Row (modifier = Modifier.padding(24.dp)) {
             Column(
                 modifier = Modifier
                     .weight(1f)
+                    .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
-            OutlinedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show More")
+            OutlinedButton(onClick = {
+                expanded = !expanded
+            }) {
+                Text(text = if (expanded) "Show Less" else "Show More")
             }
         }
     }
@@ -62,5 +97,32 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     ComposeBasicsTheme {
         MyApp()
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinuedClicked: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = { onContinuedClicked() }
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    ComposeBasicsTheme {
+        OnboardingScreen(onContinuedClicked = {})
     }
 }
