bb79eece8d753071264610a5c59043437f5ac680
김재희 <jaehee.kim@hyundai.com>
2022-10-11 17:28:25 +0900

    1st SplashScreen 방법

--- SplashScreenAndroid12/app/src/main/java/com/my/samplecode/splashscreenandroid12/MainActivity.kt
+++ SplashScreenAndroid12/app/src/main/java/com/my/samplecode/splashscreenandroid12/MainActivity.kt
@@ -10,11 +10,14 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.my.samplecode.splashscreenandroid12.ui.theme.SplashScreenAndroid12Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+
         setContent {
             SplashScreenAndroid12Theme {
                 // A surface container using the 'background' color from the theme
