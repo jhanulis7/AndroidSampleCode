471ef2a5d4ed5ffcfb6a0509403349ceddd94718
김재희 <jaehee.kim@hyundai.com>
2022-10-11 16:04:59 +0900

    코드정리

--- ComposeRippleComponent/app/src/main/java/com/my/samplecode/composeripplecomponent/MainActivity.kt
+++ ComposeRippleComponent/app/src/main/java/com/my/samplecode/composeripplecomponent/MainActivity.kt
@@ -3,11 +3,7 @@ package com.my.samplecode.composeripplecomponent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.*
-import androidx.compose.animation.core.FastOutLinearInEasing
-import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -19,12 +15,13 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.ripple.rememberRipple
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -58,9 +55,9 @@ fun Greeting(name: String) {
     ) {
         Text(text = "Click Effect Component")
         Spacer(modifier = Modifier.padding(top = 30.dp))
-        BounceEffect("Bounce")
+        PulsateEffect("Pulsate")
         Spacer(modifier = Modifier.padding(top = 20.dp))
-        BounceEffect("Click")
+        BounceEffect("Bounce")
         Spacer(modifier = Modifier.padding(top = 20.dp))
         ShakeEffect("Shake")
         Spacer(modifier = Modifier.padding(top = 20.dp))
--- ComposeRippleComponent/app/src/main/java/com/my/samplecode/composeripplecomponent/RippleEffect.kt
+++ ComposeRippleComponent/app/src/main/java/com/my/samplecode/composeripplecomponent/RippleEffect.kt
@@ -1,27 +1,18 @@
 package com.my.samplecode.composeripplecomponent
 
-import androidx.compose.animation.*
 import androidx.compose.animation.core.*
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
-import androidx.compose.material.ripple.rememberRipple
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.composed
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalDensity
 
 
 /**
