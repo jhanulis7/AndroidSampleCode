5abdf9de0a79c88e9b777afc79b9cf3d9a9bd2fe
김재희 <jaehee.kim@42dot.ai>
2022-11-10 17:27:35 +0900

    애니메이션 및 M3

---
+++ AlgorithmMatrial3/app/src/main/java/com/my/sample/algorithmmatrial3/ui/compose/CLazyColumn.kt
@@ -0,0 +1,104 @@
+package com.my.sample.algorithmmatrial3.ui.compose
+
+import com.my.sample.algorithmmatrial3.R
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun CLazyColumn(
+    modifier: Modifier = Modifier,
+    titleList: List<String>,
+    clickEvent: (index: Int) -> Unit
+) {
+
+    LazyColumn(modifier = modifier) {
+        itemsIndexed(titleList) { index , item ->
+            CLazyColumnAdapter(
+                modifier = modifier,
+                index = index,
+                title = item,
+                clickEvent = { clickEvent.invoke(index) }
+            )
+        }
+    }
+}
+
+@Composable
+fun CLazyColumnAdapter(
+    modifier: Modifier = Modifier,
+    index: Int,
+    title: String,
+    subTit
+    clickEvent: () -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .padding(2.dp) // card 와 card 사이 padding
+            .fillMaxWidth()
+            .clickable {
+                clickEvent.invoke()
+            },
+        elevation = CardDefaults.outlinedCardElevation(),
+        shape = RoundedCornerShape(12.dp)
+    ) {
+        Row(modifier = Modifier.padding(
+            start = 5.dp,
+            top = 5.dp,
+            bottom = 5.dp
+        ), //card 안에서의 row padding
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(5.dp) //row 내의 뷰들을 space 조절
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(width = 28.dp, height = 28.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, Color.Transparent, CircleShape)
+                    .background(Color(R.color.icon_blue))
+            ) {
+                Text(
+                    text = "${index + 1}",
+                    modifier = Modifier.align(Alignment.Center),
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Column {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1, //maxLines 매개변수
+                    overflow = TextOverflow.Ellipsis //텍스트 오버플로
+                )
+                Text(
+                    text = description,
+                    style = MaterialTheme.typography.body2,
+                    fontSize = 10.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        }
+    }
+}
---
+++ Matrial3Sample/app/src/main/java/com/my/sample/matrial3sample/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.my.sample.matrial3sample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.my.sample.matrial3sample.ui.theme.Matrial3SampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Matrial3SampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Matrial3SampleTheme {
+        Greeting("Android")
+    }
+}
