f5d974a52438b2d5711e515dcf57d23a6c8e1c22
김재희 <jaehee.kim@42dot.ai>
2023-02-14 23:47:10 +0900

    compose navi 구현

--- NavigationSample/app/src/main/java/com/example/navigationsample/MainActivity.kt
+++ NavigationSample/app/src/main/java/com/example/navigationsample/MainActivity.kt
@@ -6,10 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigationsample.ui.navigate.NavRoutes
+import com.example.navigationsample.ui.screen.Home
+import com.example.navigationsample.ui.screen.Profile
+import com.example.navigationsample.ui.screen.Welcome
 import com.example.navigationsample.ui.theme.NavigationSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -30,14 +36,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = NavRoutes.Home.routes) {
+
+        composable(NavRoutes.Home.routes) {
+            Home(navController = navController)
+        }
+
+        composable(NavRoutes.Welcome.routes + "/{userName}") { backStackEntry ->
+            val userName = backStackEntry.arguments?.getString("userName")
+            Welcome(navController = navController, userName)
+        }
+
+        composable(NavRoutes.Profile.routes) {
+            Profile(navController = navController)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     NavigationSampleTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
